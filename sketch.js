87710ea39da7a42b1a4cf458a56a3b57ccc18b84
sanjay <sanjaylakshan08@gmail.com>
2021-07-31 18:53:54 +0530

    changes in size of platform

--- sketch.js
+++ sketch.js
@@ -17,8 +17,8 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 200, 170);
-    
+    platform = new Ground(150, 305, 200, 160);
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
